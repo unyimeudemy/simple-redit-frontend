36595732a32eb46602f0d12a33da11a6203e8c18
unyimeudoh <unyimeudemy20@gmail.com>
2023-08-07 13:09:46 +0100

    vercel

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -77,8 +77,8 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
   }
 
   return {
-    // url: "http://localhost:4000/graphql",
-    url: "https://redit-clone-backend-7bps.onrender.com",
+    url: "http://localhost:4000/graphql",
+    // url: "https://redit-clone-backend-7bps.onrender.com",
     fetchOptions: {
       credentials: "include" as const,
       headers: cookie ? { cookie } : undefined,
