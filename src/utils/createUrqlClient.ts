f0993462a39180e7cd541247b23a5f4b649a45d5
unyimeudoh <unyimeudemy20@gmail.com>
2023-08-07 13:14:50 +0100

    vercel

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -68,6 +68,7 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
   let cookie = "";
   if (isServer()) {
     // cookie = ctx.req.headers.cookie;
+    // cookie = ctx.req.headers.cookie;
   }
 
   function invalidateAllPost(cache: Cache) {
